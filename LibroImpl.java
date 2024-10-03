57d89e9095d300246a0c75ee686ac9a0c7438d6c
Cristian2373 <ca928286@gmail.com>
2024-10-02 22:16:05 -0600

    git add

---
+++ LibroImpl.java
@@ -0,0 +1,5 @@
+public class LibroImpl extends Libro {
+    public LibroImpl(String codigo, String titulo, String autor, int numPaginas, String editorial, String isbn, int anoPublicacion, int unidadesDisponibles) {
+        super(codigo, titulo, autor, numPaginas, editorial, isbn, anoPublicacion, unidadesDisponibles);
+    }
+}
---
+++ Revista.java
@@ -0,0 +1,21 @@
+public class Revista extends Material {
+    private String editorial;
+    private String periodicidad;
+    private String fechaPublicacion;
+    private int unidadesDisponibles;
+
+    public Revista(String codigo, String titulo, String editorial, String periodicidad, String fechaPublicacion, int unidadesDisponibles) {
+        super(codigo, titulo);
+        this.editorial = editorial;
+        this.periodicidad = periodicidad;
+        this.fechaPublicacion = fechaPublicacion;
+        this.unidadesDisponibles = unidadesDisponibles;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("Código: " + codigo + "\nTítulo: " + titulo + "\nEditorial: " + editorial +
+                "\nPeriodicidad: " + periodicidad + "\nFecha de publicación: " + fechaPublicacion +
+                "\nUnidades disponibles: " + unidadesDisponibles);
+    }
+}
